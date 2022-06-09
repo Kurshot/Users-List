ff49611b3648a0c7b70df2c1c3bf1b12cfe11b40
kurshot <sonkurahmetkursat@gmail.com>
2022-06-09 16:13:55 +0300

    js update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,27 @@
-let input = document.querySelector('#searchInput');
+let arrUsers = document.querySelectorAll('.users__userName');
+let searchInput = document.querySelector('#searchInput');
+
+//console.log(arrUsers.length);
 
 
-input.addEventListener('change', find);
+function myFunc(){
 
+    // console.log(searchInput.value);
+    for(i = 0; i < arrUsers.length; i++){
+        arrUsers[i].parentElement.parentElement.parentElement.style.display = "";
+    }
+
+    hidingUsers = []; // Girilen değeri İÇERMEYEN isimleri tutar.
+    
+    for(i = 0; i < arrUsers.length ; i++){
+        if( !( arrUsers[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase()) ) ){
+            hidingUsers.push(arrUsers[i]);
+        }
+    }
+
+    for(i = 0; i < hidingUsers.length; i++){
+        hidingUsers[i].parentElement.parentElement.parentElement.style.display = "none";
+    }
+}
+
+searchInput.addEventListener('input', myFunc);
